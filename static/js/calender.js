9729ec772cc6b485b418a60da8239a964984a483
SEUNGJ0 <kuksj0312@naver.com>
2023-02-15 22:50:29 +0900

    캘린더 UI 구현 및 수정

--- static/js/calender.js
+++ static/js/calender.js
@@ -1,89 +1,117 @@
-let date = new Date();
+const date = new Date();
+const monthNames = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"];
+const daysOfWeek = ["일", "월", "화", "수", "목", "금", "토"];
 
-const renderCalender = () => {
-    const viewYear = date.getFullYear();
-    const viewMonth = date.getMonth();
+// 현재 달력의 년도와 월 출력
+const yearElem = document.querySelector('.year');
+const monthElem = document.querySelector('.month');
 
-    document.querySelector('.year').textContent = `${viewYear}`;
-    document.querySelector('.month').textContent = `${viewMonth + 1}`;
-    
-    document.querySelector('.Select_year').textContent = `${viewYear}`;
-    document.querySelector('.Select_month').textContent = `${viewMonth + 1}`;
+function setYearMonth(year, month) {
+    yearElem.textContent = year;
+    monthElem.textContent = monthNames[month];
+}
 
-    const prevLast = new Date(viewYear, viewMonth, 0);
-    const thisLast = new Date(viewYear, viewMonth + 1, 0);
+// showCalendar 함수 추가
+function showCalendar(year, month) {
+    // 현재 달력의 년도와 월 출력
+    setYearMonth(year, month);
 
-    const PLDate = prevLast.getDate();
-    const PLDay = prevLast.getDay();
-
-    const TLDate = thisLast.getDate();
-    const TLDay = thisLast.getDay();
-
-    const prevDates = [];
-    const thisDates = [...Array(TLDate + 1).keys()].slice(1);
-    const nextDates = [];
-
-    if (PLDay !== 6) {
-        for (let i = 0; i < PLDay + 1; i++) {
-            prevDates.unshift(PLDate - i);
+    // 달력에 날짜 채우기
+    const datesElem = document.querySelector('.dates');
+    datesElem.innerHTML = '';
+    const currentMonthDays = new Date(year, month + 1, 0).getDate();
+    const currentMonthFirstDay = new Date(year, month, 1).getDay();
+    const today = new Date();
+    for (let i = 0; i < currentMonthFirstDay; i++) {
+        datesElem.innerHTML += '<div class="date"></div>';
+    }
+    for (let i = 1; i <= currentMonthDays; i++) {
+        const dateElem = document.createElement('div');
+        dateElem.classList.add('date');
+        dateElem.textContent = i;
+        dateElem.dataset.date = i;
+        if (year === today.getFullYear() && month === today.getMonth() && i === today.getDate()) {
+            dateElem.classList.add('current-date');
         }
+        datesElem.appendChild(dateElem);
     }
 
-    for (let i = 1; i < 7 - TLDay; i++) {
-        nextDates.push(i);
+    // 달력에 날짜가 없는 경우 클릭 이벤트 리스너를 등록하지 않음
+    if (datesElem.children.length === 0) {
+        return;
     }
 
-    const dates = prevDates.concat(thisDates, nextDates);
-    const firstDateIndex = dates.indexOf(1);
-    const lastDateIndex = dates.lastIndexOf(TLDate);
+    // 각 날짜를 클릭 이벤트에 바인딩
+    const dateElems = document.querySelectorAll('.date');
+    dateElems.forEach((elem) => {
+        elem.addEventListener('click', handleClickDate);
+    });
+}
 
-    dates.forEach((date, i) => {
-        const condition = i >= firstDateIndex && i < lastDateIndex + 1 ?
-            'this' :
-            'other';
-        dates[i] = `
-            <div class="date ${condition}">
 
-                <div class="date-itm">
-                    ${date}
-                </div>
-                
-                <div class="date_event">
-                    <div class="event-itm"></div>
-                </div>
+// 날짜를 클릭했을 때 실행할 함수
+function handleClickDate(event) {
 
-            </div>
-        `;
+    // 선택한 날짜
+    const selectedDateElem = event.target;
+    const selectedDate = selectedDateElem.dataset.date;
+    const selectedYear = yearElem.textContent;
+    const selectedMonth = monthNames.indexOf(monthElem.textContent);
+
+    // 기존에 선택한 날짜가 있다면 회색 배경을 없애기
+    const selectedElems = document.querySelectorAll('.selected-date');
+    selectedElems.forEach((elem) => {
+        elem.classList.remove('selected-date');
     });
 
-    document.querySelector('.dates').innerHTML = dates.join('');
+    // 선택한 날짜에 회색 배경 추가
+    selectedDateElem.classList.add('selected-date');
 
-    const today = new Date();
-    if (viewMonth === today.getMonth() && viewYear === today.getFullYear()) {
-        for (let date of document.querySelectorAll('.date-itm')) {
-            if (+date.innerText === today.getDate()) {
-                date.parentNode.classList.add('today');
-                break;
+    // 선택한 날짜 표시
+    document.querySelector('.Select_dates').textContent = selectedDate;
+    document.querySelector('.Select_year').textContent = selectedYear;
+    document.querySelector('.Select_month').textContent = selectedMonth;
+    document.querySelector('.resv-wrapper').style.display = 'block';
+}
 
-            }
-        }
+
+// 예약 창 닫기
+const closeBtn = document.querySelector('.resv-close');
+closeBtn.addEventListener('click', () => {
+    document.querySelector('.resv-wrapper').style.display = 'none';
+});
+
+// 이전달/다음달/오늘 버튼에 이벤트 리스너 등록
+const prevBtn = document.querySelector('.go-prev');
+const nextBtn = document.querySelector('.go-next');
+const todayBtn = document.querySelector('.go-today');
+
+// 이전달
+prevBtn.addEventListener('click', () => {
+    const year = Number(yearElem.textContent);
+    const month = monthNames.indexOf(monthElem.textContent) - 1;
+    if (month < 0) {
+        showCalendar(year - 1, 11);
+    } else {
+        showCalendar(year, month);
     }
+});
+  
+// 다음달
+nextBtn.addEventListener('click', () => {
+const year = Number(yearElem.textContent);
+const month = monthNames.indexOf(monthElem.textContent) + 1;
+if (month > 11) {
+    showCalendar(year + 1, 0);
+} else {
+    showCalendar(year, month);
+}
+});
 
-};
+// 오늘
+todayBtn.addEventListener('click',() => {
+    const today = new Date();
+    showCalendar(today.getFullYear(), today.getMonth());
+});
 
-renderCalender();
-
-const prevMonth = () => {
-    date.setMonth(date.getMonth() - 1);
-    renderCalender();
-};
-
-const nextMonth = () => {
-    date.setMonth(date.getMonth() + 1);
-    renderCalender();
-};
-
-const goToday = () => {
-    date = new Date();
-    renderCalender();
-};
+showCalendar(date.getFullYear(), date.getMonth());
--- static/js/reserve.js
+++ static/js/reserve.js
@@ -1,54 +1,32 @@
-const resvTab = document.querySelector('.resv-wrapper');
-const exitBtn = document.querySelector('.resv-close');
-exitBtn.addEventListener('click', ()=>{resvTab.classList.remove('open');});
+// 'resv-wrapper' element
+const resvWrapper = document.querySelector('.resv-wrapper');
 
+// 'dates' element
+const dates = document.querySelector('.dates');
 
-// 날짜별로 이벤트 등록용 함수 및 변수
-const selDate = []
-const dateFunc = ()=>{
-    const dates = document.querySelectorAll('.date');
-    const year = document.querySelector('.year');
-    const month = document.querySelector('.month');
-    dates.forEach((i)=>{
-        i.addEventListener('click', ()=>{
-            if(i.classList.contains('other') || i.classList.contains('selected')){
-                dates.forEach((ig)=>{ig.classList.remove('selected');});
-                i.classList.remove('selected');
-                selDate.length=0;
-                
-            }else if(selDate.length > 0){
-                dates.forEach((ig)=>{ig.classList.remove('selected');});
-                selDate.length=0;
-                i.classList.add('selected');
-                selDate.push([year.innerHTML, month.innerHTML, i.innerHTML]);
-                resvTab.classList.add('open');
+// Add click event listener to each date element
+dates.addEventListener('click', (event) => {
+    // Get the selected date
+    const selectedDate = event.target.getAttribute('data-date');
+    const selectedYear = document.querySelector('.year').innerHTML;
+    const selectedMonth = document.querySelector('.month').innerHTML;
 
-            }else{
-                i.classList.add('selected');
-                selDate.push([year.innerHTML, month.innerHTML, i.innerHTML]);
-                resvTab.classList.add('open');
-            }
-        });
-    });
-};
+    // Set the date information to the reservation window
+    const selectYearElem = document.querySelector('.Select_year');
+    const selectMonthElem = document.querySelector('.Select_month');
+    const selectDateElem = document.querySelector('.Select_dates');
 
-// 초기화 함수 
-const reset = ()=>{
-    selDate.length=0;
-    dateFunc();
-}
+    selectYearElem.innerHTML = selectedYear;
+    selectMonthElem.innerHTML = selectedMonth;
+    selectDateElem.innerHTML = selectedDate;
 
-// 로드시 Nav 버튼들 이벤트 등록 및 초기화
-window.onload=()=>{
-    const navBtn = document.querySelectorAll('.nav-btn');
-    navBtn.forEach(inf=>{
-        if(inf.classList.contains('go-prev')){
-            inf.addEventListener('click', ()=>{prevMonth(); reset();});
-        }else if(inf.classList.contains('go-today')){
-            inf.addEventListener('click', ()=>{goToday(); reset();});
-        }else if(inf.classList.contains('go-next')){
-            inf.addEventListener('click', ()=>{nextMonth(); reset();});
-        }
-    });
-    reset();
-}
+    // Show the reservation window
+    resvWrapper.style.display = 'block';
+});
+
+// Add click event listener to the close button
+const resvCloseBtn = document.querySelector('.resv-close');
+resvCloseBtn.addEventListener('click', () => {
+    // Hide the reservation window
+    resvWrapper.style.display = 'none';
+});
